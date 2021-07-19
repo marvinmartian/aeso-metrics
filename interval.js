7ee3221d9926febf0bdbf2a412178d11f290faee
Melvin <marvinmartian@gmail.com>
2021-07-18 20:51:52 -0600

    Remove unnecessary console.log

--- interval.js
+++ interval.js
@@ -6,13 +6,9 @@ const http = require('http')
 const url = require('url')
 
 const client = require('prom-client');
-const { exit } = require("process");
-const { table } = require("console");
-const collectDefaultMetrics = client.collectDefaultMetrics;
+// const collectDefaultMetrics = client.collectDefaultMetrics;
 const Registry = client.Registry;
 const register = new Registry();
-
-// collectDefaultMetrics({ register });
 
 register.setDefaultLabels({
   app: 'alberta-energy'
@@ -23,27 +19,6 @@ const poolprice_stats = new client.Gauge({
   help: 'Pool price per MWh'
 });
 register.registerMetric(poolprice_stats);
-
-const generation_mc_stats = new client.Gauge({
-    name: 'generation_mc',
-    help: 'Generation Maximum Capacity',
-    labelNames: ['group']
-  });
-register.registerMetric(generation_mc_stats);
-
-const generation_tng_stats = new client.Gauge({
-    name: 'generation_tng',
-    help: 'Generation Total Net Generation',
-    labelNames: ['group']
-  });
-register.registerMetric(generation_tng_stats);
-
-const generation_dnc_stats = new client.Gauge({
-    name: 'generation_dnc',
-    help: 'Generation Dispatched (and Accepted) Contingency Reserve',
-    labelNames: ['group']
-  });
-register.registerMetric(generation_dnc_stats);
 
 // ##############
 
@@ -78,7 +53,6 @@ const dcr_stats = new client.Gauge({
 register.registerMetric(dcr_stats);
 
 function captureInterchangeMetrics(stat_object) {
-  // console.log(stat_object);
   for ( key in stat_object) {
     let asset_name = key;
     let metric_value = parseInt(stat_object[key]);
@@ -86,11 +60,9 @@ function captureInterchangeMetrics(stat_object) {
       interchange_stats.set({location: asset_name },metric_value);
     }
   }
-  
 }
 
 function captureMetrics(stat_object,asset_type,category) { 
-  // console.log(stat_object);
   for ( key in stat_object) {
     let asset_name = key;
     let asset_category = category || "";
@@ -108,12 +80,6 @@ function captureMetrics(stat_object,asset_type,category) {
       dcr_stats.set({asset: asset_name, type: asset_type, category: asset_category },dcr_metric_value);
     }
   }
-  // metric.set({asset: asset_name, type: asset_type, category: category },mc_stat);
-}
-
-// Helper Function(s)
-function isUpperCase(str) {
-  return str === str.toUpperCase();
 }
 
 async function getCurrentPoolPrice() {
@@ -121,50 +87,41 @@ async function getCurrentPoolPrice() {
   headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36' }});
 
   var data = response.data;
-  // console.log(data);
   if ( data.poolprice && data.poolprice instanceof Array && data.poolprice.length > 0 && data.poolprice[data.poolprice.length -1][1]) {
     poolprice_stats.set(data.poolprice[data.poolprice.length -1][1]);  
   } else { 
     console.log('Error setting pool price',data.poolprice);
   }
-  // poolprice_stats.set(data.poolprice[data.poolprice.length -1][1]);
-  // console.log(data.poolprice[data.poolprice.length -1][1]);
 }
 
 async function getCurrentStats() {
     let response = await axios( { url: "http://ets.aeso.ca/ets_web/ip/Market/Reports/CSDReportServlet", 
       headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36' }});
-    // console.log("Response:", response.data);
     var data = response.data;
     var $ = cheerio.load(data);
     var aeso_stats_array = {};
 
     function summary_table_parser(table_to_parse) {
-      // console.log('SUMMARY')
       let stats_array = {}
 
       table_to_parse.each(function (i, elem) {
         if ( i > 1 ) {
           let row_data = $(this).children('td').toArray()
-          // console.log( $(row_data[0]).text().replace(/[^\w\s]/gi, '').replace(/ /g,"_"), parseInt($(row_data[1]).text()) );
           stats_array[$(row_data[0]).text().replace(/[^\w\s]/gi, '').replace(/ /g,"_")] = $(row_data[1]).text()
         }
 
       });
-      // console.log(stats_array);
       return stats_array;
     }
 
     // ###### Parse the generation table type
     function generation_table_parser(table_to_parse) {
-      // console.log('generation')
       let stats_array = {}
 
       table_to_parse.each(function (i, elem) {
         if ( i > 1 ) {
           let row_data = $(this).children('td').toArray()
           stats_array[$(row_data[0]).text().replace(/[^\w\s]/gi, '').replace(/ /g,"_")] = { 'MC': $(row_data[1]).text(), 'TNG': $(row_data[2]).text(), 'DCR': $(row_data[3]).text() }
-          // console.log( $(row_data[0]).text().replace(/[^\w\s]/gi, '').replace(/ /g,"_"), parseInt($(row_data[1]).text()) );
         }
 
       });
@@ -174,29 +131,24 @@ async function getCurrentStats() {
 
     // ###### Parse the interchange table type
     function interchange_table_parser(table_to_parse) {
-      // console.log('interchange')
       let stats_array = {}
 
       table_to_parse.each(function (i, elem) {
         if ( i > 1 ) {
           let row_data = $(this).children('td').toArray()
-          // console.log( $(row_data[0]).text().replace(/[^\w\s]/gi, '').replace(/ /g,"_"), parseInt($(row_data[1]).text()) );
           stats_array[$(row_data[0]).text().replace(/[^\w\s]/gi, '').replace(/ /g,"_")] = $(row_data[1]).text()
         }
 
       });
-      // console.log(stats_array);
       return stats_array;
     }
 
     // ###### Parse the gas table type
     function gas_table_parser(table_to_parse) {
-      // console.log('gas')
       let stats_array = { 'Simple': {}, 'Cogeneration': {}, 'Combined': {} };
       table_to_parse.each(function (i, elem) {
         
         let row_data = $(this).children('td').toArray()
-        // let row_name = $(row_data[0]).text().replace(/[^a-zA-Z0-9 ]/g, "").replace(/ /g,"_");
         let row_name = $(row_data[0]).text();
         switch (row_name) {
           case 'Simple Cycle':
@@ -214,7 +166,6 @@ async function getCurrentStats() {
         }
 
         if ( row_data.length > 1 && $(row_data[0]).text() != "ASSET" ) {
-          // console.log(i,active,row_name);
           let asset_name = $(row_data[0]).text();
           let mc_stat  = parseInt($(row_data[1]).text()); 
           let tng_stat = parseInt($(row_data[2]).text());
@@ -223,38 +174,31 @@ async function getCurrentStats() {
             console.log("ERROR",asset_name,mc_stat,tng_stat,dnr_stat);
           } else { 
             stats_array[active][$(row_data[0]).text().replace(/[^\w\s]/gi, '').replace(/ /g,"_")] = { 'MC': $(row_data[1]).text(), 'TNG': $(row_data[2]).text(), 'DCR': $(row_data[3]).text() }
-            // gas_tng_stats.set({asset: asset_name, type: `${active}` },tng_stat);
-            // gas_dnc_stats.set({asset: asset_name, type: `${active}` },dnr_stat);
           }
           
         } 
       
       });
-      // console.log(stats_array);
       return stats_array;
     }
 
     // ###### Parse the default table type
     function default_table_parser(table_to_parse) {
-      // console.log('default','-',electricity_type)
       let stats_array = {}
 
       table_to_parse.each(function (i, elem) {
         if ( i > 1 ) {
           let row_data = $(this).children('td').toArray()
-          // console.log( $(row_data[0]).text(),parseInt($(row_data[1]).text()),parseInt($(row_data[2]).text()),parseInt($(row_data[3]).text()) );
           stats_array[$(row_data[0]).text().replace(/[^\w\s]/gi, '').replace(/ /g,"_")] = { 'MC': $(row_data[1]).text(), 'TNG': $(row_data[2]).text(), 'DCR': $(row_data[3]).text() }
         }
         
       });
-      // console.log(stats_array);
       return stats_array;
     }
 
     var the_tables = $('table [border="1"]')
     the_tables.each(function (i, elem) {
       var table_title = $(this).find('tr').first().text().trim().toLowerCase().trim().replace(/ /g,"_");
-      // console.log(table_title);
 
       var table_data = $(this).find('tr');
       switch (table_title.toLowerCase()) {
@@ -270,7 +214,6 @@ async function getCurrentStats() {
           break;
         case 'gas':
           aeso_stats_array[table_title] = gas_table_parser(table_data)
-          // console.log(aeso_stats_array[table_title]);
           captureMetrics(aeso_stats_array[table_title]["Combined"],table_title,"Combined");
           captureMetrics(aeso_stats_array[table_title]["Cogeneration"],table_title,"Cogeneration");
           captureMetrics(aeso_stats_array[table_title]["Simple"],table_title,"Simple");
@@ -281,13 +224,7 @@ async function getCurrentStats() {
           break;
       }
 
-      
-      // console.log('--------------');
-
     });
-    // console.log(aeso_stats_array)
-    console.log('----------------------');
-    // process.exit();
 } 
 
 function callStatsScraperEveryNSeconds(n) {
